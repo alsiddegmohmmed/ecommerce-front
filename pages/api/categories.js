4ca6d50696cf77c45570e8d141941a4bf6278b5a
Al Siddeg Omer <alsiddeg.omer19990@gmail.com>
2024-07-03 15:16:08 +0300

    update

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,6 +1,5 @@
-// api/categories.js
-
 import { Product } from '@/models/Product';
+import { Category } from '@/models/Category'; // Ensure Category model is imported
 import { mongooseConnect } from '@/lib/mongoose';
 
 export default async function handler(req, res) {
@@ -14,9 +13,10 @@ export default async function handler(req, res) {
 
   try {
     const products = await Product.find(query).populate('category');
+    res.setHeader('Cache-Control', 'no-store'); // Disable caching
     res.status(200).json(products);
   } catch (error) {
     console.error('Error fetching products:', error);
-    res.status(500).json({ error: `Error fetching products: ${error.message}` });
+    res.status(500).json({ error: 'Error fetching products' });
   }
 }
